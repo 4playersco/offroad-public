eae3fb98f70b40a58d8397326b90432a88907a6b
Craig Freeman <craigfreeman@gmail.com>
2025-03-25 22:06:45 -0600

    forms

--- src/components/common/ContactForm/index.tsx
+++ src/components/common/ContactForm/index.tsx
@@ -1,14 +1,14 @@
 import { useCallback, useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useNavigate } from "react-router";
 
 import styles from "./ContactForm.module.scss";
 import Button from "../../utility/Button";
 import Captcha from "../Captcha";
 
-const { VITE_NODE_ENV } = import.meta.env;
-const isDev = VITE_NODE_ENV === "development";
-
 const ContactForm = () => {
-  const [validRecaptcha, setValidRecaptcha] = useState(isDev);
+  const navigate = useNavigate();
+  const [validRecaptcha, setValidRecaptcha] = useState(false);
 
   const handleCaptchaChange = useCallback(
     (isValid: boolean) => {
@@ -17,43 +17,111 @@ const ContactForm = () => {
     [setValidRecaptcha]
   );
 
-  const isDisabled = !validRecaptcha;
+  const encode = useCallback((data: { [key: string]: string }) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }, []);
 
   return (
     <div className={styles.ContactForm}>
-      <form name="contact" method="POST" action="/thanks" data-netlify="true">
-        <div className={styles.formFields}>
-          <input
-            className={styles.field}
-            placeholder="Name"
-            type="name"
-            name="name"
-            required
-          />
-          <input
-            className={styles.field}
-            placeholder="Email"
-            type="email"
-            name="email"
-            required
-          />
-          <textarea
-            className={styles.field}
-            placeholder="Message"
-            name="message"
-            required
-          />
-          {!isDev && (
-            <div className={styles.recaptcha}>
-              <Captcha onChange={handleCaptchaChange} />
-            </div>
-          )}
-          <input type="hidden" name="form-name" value="contact" />
-        </div>
-        <Button type="submit" disabled={isDisabled}>
-          Send
-        </Button>
-      </form>
+      <Formik
+        initialValues={{ name: "", email: "", message: "" }}
+        validate={(values) => {
+          const errors: { email?: string } = {};
+          if (!values.email) {
+            errors.email = "Required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting, setStatus }) => {
+          fetch("/", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: encode({
+              "form-name": "contact",
+              ...values,
+            }),
+          })
+            .then(() => {
+              setSubmitting(false);
+              navigate("/thanks");
+            })
+            .catch(() => {
+              setSubmitting(false);
+              setStatus({
+                msg: "There was a problem submitting your message. Please try again later.",
+              });
+            });
+        }}
+      >
+        {({ isSubmitting, handleSubmit }, ...props) => {
+          const isDisabled = !validRecaptcha || isSubmitting;
+
+          return (
+            <Form
+              name="contact"
+              method="post"
+              action="/thanks"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              onSubmit={handleSubmit}
+              {...props}
+            >
+              <div className={styles.formFields}>
+                <Field
+                  className={styles.field}
+                  placeholder="Name"
+                  type="name"
+                  name="name"
+                />
+                <ErrorMessage
+                  className={styles.error}
+                  name="name"
+                  component="div"
+                />
+                <Field
+                  className={styles.field}
+                  placeholder="Email"
+                  type="email"
+                  name="email"
+                />
+                <ErrorMessage
+                  className={styles.error}
+                  name="email"
+                  component="div"
+                />
+                <Field
+                  className={styles.field}
+                  placeholder="Message"
+                  type="message"
+                  component="textarea"
+                  name="message"
+                />
+                <ErrorMessage
+                  className={styles.error}
+                  name="message"
+                  component="div"
+                />
+                <div className={styles.recaptcha}>
+                  <Captcha onChange={handleCaptchaChange} />
+                </div>
+              </div>
+              <Button type="submit" disabled={isDisabled}>
+                Send
+              </Button>
+            </Form>
+          );
+        }}
+      </Formik>
     </div>
   );
 };
